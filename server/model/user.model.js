3e306d38cf3fdb76c05484e533061bd315ee991c
Devprodipraj <devprodipraj@gmail.com>
2025-05-23 22:08:40 +0600

    connec data base

---
+++ server/model/user.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import validator from "validator"
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        require :  true
+    },
+    email: {
+        type : String,
+        require : true,
+        unique : true,
+        validate : [validator.isEmail, "Please Enter Valid Email Address..."]
+    },
+    phone: {
+        type : Number,
+        require : true,
+    },
+    photo : {
+        type : String,
+        require : true,
+    },
+    role :{
+        type : String,
+        require : true,
+        enum:["user", "admin"],
+        default : "student",
+    },
+    password : {
+        type : String,
+        require : true,
+        minlength : 8,
+
+    },
+    createdAt: {
+        type : Date,
+        default: Date.now,
+    }
+})
+
+
+export const User = mongoose.model("User", userSchema);
--- server/server.js
+++ server/server.js
@@ -1,7 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express"
+import dotenv from "dotenv"
+import { connectDB } from "./config/mongoDB.js"
 
+
+// config dotenv for import evn  
+dotenv.config()
+
+// database connection 
+connectDB()
+
+
+const app = express()
+const port = process.env.PORT || 4001;
+
+// root path create 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
