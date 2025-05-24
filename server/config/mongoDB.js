07a6d885df9b884bda000ff9ddc586cdb6985bac
DevProdipraj <devprodipraj@gmail.com>
2025-05-24 10:50:18 +0600

    fixed mongo db connection error

--- server/config/mongoDB.js
+++ server/config/mongoDB.js
@@ -1,11 +1,18 @@
-import mongoose  from "mongoose";
+
+import dotenv from 'dotenv';
+dotenv.config(); 
+import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Database Connected..")
-    } catch (error) {
-        console.log(`MongoDB Connection Error: ${error.message}`)
-        process.exit(1)
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`Database Connected..  `);
+  } catch (error) {
+    console.error(`MongoDB Connection Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+
+
