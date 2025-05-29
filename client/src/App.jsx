99803d410ec62e1b1b9cb18c109b826b26935dfb
DevProdipraj <devprodipraj@gmail.com>
2025-05-29 17:33:37 +0600

    crate add page && complete all route useing react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Blog from './pages/Blog'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashbord from './pages/Dashbord'
+import Navbar from './component/Navbar'
+import Footer from './component/Footer'
 
 const App = () => {
+  const location = useLocation();
+  const hideLayoutPaths = ['/login', '/register', '/dashbord'];
+  const shouldHideLayout = hideLayoutPaths.includes(location.pathname.toLowerCase());
   return (
     <div>
-      <h1 className='text-4xl text-orange-700'>Hello World</h1>
+      {!shouldHideLayout && <Navbar />}
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/Blog' element={<Blog/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/dashbord' element={<Dashbord/>}/>
+
+    </Routes>
+    {!shouldHideLayout && <Footer/> }
     </div>
   )
 }
